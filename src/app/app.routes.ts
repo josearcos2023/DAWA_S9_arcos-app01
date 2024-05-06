4d4db7d80ec736921dcb805b5c93f17fab5245d6
josearcos2023 <jose.arcos@tecsup.edu.pe>
2024-05-06 10:51:36 -0500

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ProductoComponent } from './producto/producto.component';
+import { ProveedorComponent } from './proveedor/proveedor.component';
+import { FacturaComponent } from './factura/factura.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'cliente',
+        component: ClienteComponent
+    },
+    {
+        path:'proveedor',
+        component:ProveedorComponent
+    },
+    {
+        path:'producto',
+        component:ProductoComponent
+    },
+    {
+        path:'factura',
+        component:FacturaComponent
+    },
+    {
+        path:'empleado',
+        component:EmpleadoComponent
+    }
+
+];
---
+++ src/app/factura/factura.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factura',
+  standalone: true,
+  imports: [],
+  templateUrl: './factura.component.html',
+  styleUrl: './factura.component.css'
+})
+export class FacturaComponent {
+
+}
